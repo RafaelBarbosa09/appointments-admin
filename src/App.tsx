a14a4b44671f1288364cdb2177f3a5bbdbc10a9e
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2023-09-02 20:31:14 -0300

    feat: adiciona rotas ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { GlobalStyles } from "./styles/global";
+import Home from "./pages/Home";
+import Appointments from "./pages/Appointments";
 
 const App = () => {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointments" element={<Appointments />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
     </>
   );
---
+++ src/pages/Appointments.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const Appointments = () => {
+  return (
+    <>
+      <h1>Appointments</h1>
+      <Link to="/">Home</Link>
+    </>
+  );
+}
+
+export default Appointments;
