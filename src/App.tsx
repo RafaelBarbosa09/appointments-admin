81290592d0b09889867ae4bee54dd78d7a3c0dcb
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2023-09-02 20:32:21 -0300

    remove import nao utilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyles } from "./styles/global";
 import Home from "./pages/Home";
 import Appointments from "./pages/Appointments";
